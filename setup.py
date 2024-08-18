df609c5a99bf1583d454b89093f75d5aa71ec5ee
Saurav Sabu <saurav.sabu9@gmail.com>
2024-08-18 22:57:31 +0530

    added code in setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="loan-status-prediction",
+    version="0.0.1",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages()
+)
